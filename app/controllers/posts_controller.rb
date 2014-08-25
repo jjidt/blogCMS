65f3657573a4a94d5ce5007c0ddbd05e64aa12a5
J.J <johnidt@gmail.com>
2014-08-24 22:35:30 -0700

    add scope in post model to sort all by recent

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.recent
     render('/posts/index.html.erb')
   end
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ActiveRecord::Base
   has_many :comments
   validates :title, presence: true
   validates :content, presence: true
+
+  scope :recent, lambda { order("posts.created_at DESC") }
 end
