8407742af75effa7e8119f4561236a7a9a69a785
J.J <johnidt@gmail.com>
2014-08-25 13:10:08 -0700

    add scope to sort comments by recent

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,6 @@ class Comment < ActiveRecord::Base
   belongs_to :post
   validates :body, presence: true
   validates :commenter, presence: true
+
+  scope :recent, lambda { order("comments.created_at DESC") }
 end
