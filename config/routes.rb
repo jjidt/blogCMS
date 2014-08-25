df6111c19a76284f50ba0aecee2c30641a0a0255
J.J <johnidt@gmail.com>
2014-08-24 22:24:51 -0700

    add error checking for post creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   match('/', {:via => :get, :to => 'posts#index'})
   match('/posts/new/', {:via => :get, :to => 'posts#new'})
+  match('/posts/', {:via => :post, :to => 'posts#create'})
 end
