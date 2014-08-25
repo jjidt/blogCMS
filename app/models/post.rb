26e55d303d3df414a2a020f63dfc2ca3edab4c0f
J.J <johnidt@gmail.com>
2014-08-25 13:44:24 -0700

    add time format method for post display

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,9 @@ class Post < ActiveRecord::Base
   validates :content, presence: true
 
   scope :recent, lambda { order("posts.created_at DESC") }
+
+  def format_date
+    time = self.created_at
+    "#{time.month}/#{time.day}/#{time.year}"
+  end
 end
--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -4,4 +4,11 @@ describe Post do
   it { should validate_presence_of :title }
   it { should validate_presence_of :content }
   it { should have_many :comments }
+
+  context '#format_date' do
+    it 'should put the created_at date into correct display format' do
+      test_post = Post.create({:title => 'test', :content => 'no content'})
+      expect(test_post.format_date).to eq "#{Time.now.month}/#{Time.now.day}/#{Time.now.year}"
+    end
+  end
 end
