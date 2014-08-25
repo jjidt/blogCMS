1c91b266e92578b6a34e0393a841c380d9a94266
J.J <johnidt@gmail.com>
2014-08-24 22:13:39 -0700

    make post submission nested

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,13 @@ class PostsController < ApplicationController
     @post = Post.new
     render('/posts/new.html.erb')
   end
+
+  def create
+    @post = Post.new(params[:post])
+    if @post.save
+      redirect_to('/')
+    else
+      render('/posts/new.html.erb')
+    end
+  end
 end
