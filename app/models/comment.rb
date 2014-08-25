4f910de29f9a43dfa8d575c722489a83084936f3
J.J <johnidt@gmail.com>
2014-08-24 20:42:11 -0700

    validate presence of commenter for comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ActiveRecord::Base
   validates :body, presence: true
+  validates :commenter, presence: true
 end
--- spec/comment_spec.rb
+++ spec/comment_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 describe Comment do
   it { should validate_presence_of :body }
+  it { should validate_presence_of :commenter }
 end
